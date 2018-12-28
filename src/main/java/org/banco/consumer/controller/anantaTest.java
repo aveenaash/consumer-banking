5df1142ac618b191894457f046904d2671194464
Abhinash Bastola <abastola@urgent.ly>
2018-12-27 20:21:08 -0500

    fixed conflict on anantaTest

--- src/main/java/org/banco/consumer/controller/anantaTest.java
+++ src/main/java/org/banco/consumer/controller/anantaTest.java
@@ -1,10 +1,9 @@
 package org.banco.consumer.controller;
 
 public class anantaTest {
-<<<<<<< Updated upstream
 // just a try
-=======
+
 
 	// todo i am changing master, dont forget to merge me to see the changes
->>>>>>> Stashed changes
+
 }
