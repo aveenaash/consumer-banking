f9c0b4cfbc1917bddbb279c20f228451d8aecf86
Abhinash Bastola <abastola@urgent.ly>
2018-12-27 20:15:24 -0500

    test merge thing

--- src/main/java/org/banco/consumer/controller/anantaTest.java
+++ src/main/java/org/banco/consumer/controller/anantaTest.java
@@ -1,5 +1,10 @@
 package org.banco.consumer.controller;
 
 public class anantaTest {
+<<<<<<< Updated upstream
 // just a try
+=======
+
+	// todo i am changing master, dont forget to merge me to see the changes
+>>>>>>> Stashed changes
 }
